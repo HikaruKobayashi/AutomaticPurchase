f8d02c207d7a8dd9f7536e8d05ba20b7fdeb61fb
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2020-08-26 15:27:21 +0900

    URLを変更する。

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import requests
+from bs4 import BeautifulSoup
 from selenium import webdriver
 
 driver = webdriver.Chrome()
-driver.get('https://www.google.co.jp')
+driver.get('https://www.supremenewyork.com/shop/new')
 driver.quit()
