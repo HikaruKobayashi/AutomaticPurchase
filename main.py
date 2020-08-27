f7e003c3ffe32525e1a29b4cf6648e26abd63fbe
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2020-08-28 00:48:02 +0900

    遷移先での自動操作もできるようになった。

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import requests
+import schedule
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+options = webdriver.ChromeOptions()
+driver = webdriver.Chrome(options=options)
 
-driver = webdriver.Chrome()
-driver.get('https://www.supremenewyork.com/shop/new')
+url='https://www.supremenewyork.com/shop/all'
+driver.get(url)
+driver.find_element_by_class_name('inner-article').click()
+nextUrl = driver.current_url
+print (nextUrl)
+driver.get(nextUrl)
+driver.find_element_by_class_name('continue').click()
 driver.quit()
