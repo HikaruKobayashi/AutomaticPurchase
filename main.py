47880bf654f1859ef9b78d1b6834fd708902e967
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2020-08-28 09:17:22 +0900

    カートに入れる操作まで指定できた。

--- main.py
+++ main.py
@@ -8,11 +8,19 @@ from selenium.webdriver.support.ui import Select
 options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(options=options)
 
-url='https://www.supremenewyork.com/shop/all'
-driver.get(url)
-driver.find_element_by_class_name('inner-article').click()
-nextUrl = driver.current_url
-print (nextUrl)
+# url='https://www.supremenewyork.com/shop/all'
+# driver.get(url)
+# driver.find_element_by_class_name('inner-article').click()
+# nextUrl = driver.current_url
+nextUrl = 'https://www.supremenewyork.com/shop/accessories/n2dcp4gwu/ulgkp3drz'
+# print (nextUrl)
 driver.get(nextUrl)
-driver.find_element_by_class_name('continue').click()
-driver.quit()
+size_element = driver.find_element_by_name('size')
+size_select_element = Select(size_element)
+# 77643 means Large Size by Supreme site
+size_select_element.select_by_value('77643')
+selector = '#add-remove-buttons > input'
+driver.find_element_by_css_selector(selector).click()
+selector = '.sidebar > #cart > .checkout'
+driver.find_element_by_css_selector(selector).click()
+# driver.quit()
